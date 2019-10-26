f49cf8da84f05b8676dc6473b0cd39f99fc5a737
maxxoto <ahmatdanis24@gmail.com>
2019-10-27 03:23:08 +0700

    Add Index Page + Setup UI

--- config/database.php
+++ config/database.php
@@ -4,13 +4,13 @@
         var $host = "localhost";
         var $username = "root";
         var $pass = "";
-        var $db = "jobseeker";
+        var $db = "jobseeke";
     }
 
     function __construct() {
         if (!isset($this->connection)) {
             
-            $this->connection = new mysqli($this->host, $this->username, $this->_pass, $this->db);
+            $this->connection = new mysqli($this->host, $this->username, $this->pass, $this->db);
             
             if (!$this->connection) {
                 echo '<div> Cannot connect to database server</div>';
--- index.php
+++ index.php
@@ -1,4 +1,106 @@
-<?php
-include 'config/database.php'
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-?>
+        <title>Job Seekers</title>
+        
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="assets/css/style.css"/>
+        <!-- Script JS Font Awesome -->
+        <script src="https://kit.fontawesome.com/941f29cdd8.js" crossorigin="anonymous"></script>
+  </head>
+    </head>
+        <body>
+
+            <div class="container-fluid shadow-sm bg-white">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container">
+                    <a class="navbar-brand" href="#">Navbar</a>
+
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    
+                            
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- <ul class="navbar-nav mr-auto mt-2 mt-lg-0">  
+                        </ul> -->
+                        <ul class="navbar-nav ml-auto">
+
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" >Employee Area</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#" >Cari Kerja</a>
+                            </li>
+
+                        </ul>
+                        
+                    </div>
+                    <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>  -->
+                    
+                    
+                    </div>
+                </nav>
+            </div>
+
+            <div class="container">
+                <div class="text-banner">
+                    <h1 class="text1"> Cari Lowongan Kerja </h1>
+                    <h1 class="text2"> Terlengkap dan Termudah </h1>
+                    
+                    <h2 class="text3"> Pasang Lowongan Sekarang Gratis </h2>
+                    
+                    <a class="btn btn-primary btn-lg custom-rounded text-white">Pasang Iklan</a>
+                    
+                </div>
+                <div class="img-banner">
+                    <img class="" src="assets/img/logo.jpeg">   
+                </div>
+
+                <div class="clear"> </div>
+
+                <div align="center" class="searchbox">
+                    <form action="#" method="post">
+                        <div class="row">
+
+                            <div class="col-md-3">
+                                <input class="form-control" name="kategori" type="text" placeholder="Kategori Pekerjaan">
+                            </div>
+
+                            <div class="col-md-3">
+                                <input class="form-control" name="posisi" type="text" placeholder="Posisi">
+                            </div>
+                                
+
+                            <div class="col-md-3">
+                                <input class="form-control" name="lokasi" type="text" placeholder="Lokasi">
+                            </div>
+                            
+                            <div class="col-md-3">
+                                <button class="btn btn-primary btn-sm" type="button" type="text"> <i class="fas fa-search fa-2x"></i> </button>
+                            </div>
+                            
+                        </div>
+                    </form>
+                </div>
+                
+            </div>
+
+        </body>
+    <!-- Optional JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</html>
